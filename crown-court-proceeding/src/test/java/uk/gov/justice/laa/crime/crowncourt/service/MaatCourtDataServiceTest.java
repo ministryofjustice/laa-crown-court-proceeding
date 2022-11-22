eaf492e70a35689888a71dce66762b51ae6b06e4
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2022-11-22 22:41:10 +0000

    LCAM-221|Improve unit test coverage

---
+++ crown-court-proceeding/src/test/java/uk/gov/justice/laa/crime/crowncourt/service/MaatCourtDataServiceTest.java
@@ -0,0 +1,45 @@
+package uk.gov.justice.laa.crime.crowncourt.service;
+
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.justice.laa.crime.crowncourt.client.MaatCourtDataClient;
+import uk.gov.justice.laa.crime.crowncourt.config.MaatApiConfiguration;
+import uk.gov.justice.laa.crime.crowncourt.data.builder.TestModelDataBuilder;
+import uk.gov.justice.laa.crime.crowncourt.dto.maatcourtdata.IOJAppealDTO;
+import uk.gov.justice.laa.crime.crowncourt.util.MockMaatApiConfiguration;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class MaatCourtDataServiceTest {
+
+    private static final String LAA_TRANSACTION_ID = "laaTransactionId";
+
+    @Mock
+    MaatCourtDataClient maatCourtDataClient;
+
+    @InjectMocks
+    private MaatCourtDataService maatCourtDataService;
+
+    @Spy
+    private MaatApiConfiguration maatApiConfiguration = MockMaatApiConfiguration.getConfiguration(1000);
+
+    @Test
+    void givenRepId_whenGetCurrentPassedIojAppealFromRepIdIsInvoked_thenResponseIsReturned() {
+        IOJAppealDTO expected = new IOJAppealDTO();
+        when(maatCourtDataClient.getApiResponseViaGET(any(), anyString(), anyMap(), any()))
+                .thenReturn(expected);
+
+        IOJAppealDTO response =
+                maatCourtDataService.getCurrentPassedIOJAppealFromRepId(TestModelDataBuilder.TEST_REP_ID, LAA_TRANSACTION_ID);
+
+        assertThat(response).isEqualTo(expected);
+    }
+
+}
