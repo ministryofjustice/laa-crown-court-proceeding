b2806b692ba961c0ca0d26f33ee7b6d9997988e6
Ahilan Santhalingam <ahilan.santhanlingam@digital.justice.gov.uk>
2022-12-29 12:57:56 +0000

    Enabling SQS Listeners based on the feature flag  + Adding Sentry sample rating of 5%

--- crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/hearing/listener/HearingResultedListener.java
+++ crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/hearing/listener/HearingResultedListener.java
@@ -2,6 +2,7 @@ package uk.gov.justice.laa.crime.crowncourt.hearing.listener;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 @Service
+@ConditionalOnProperty(value = "feature.postMvp.enabled", havingValue = "true")
 public class HearingResultedListener {
     @JmsListener(destination = "${cloud-platform.aws.sqs.queue.hearingResulted}", concurrency = "1")
     public void receive(@Payload final String message) {
