2b81fc090b3eedf1a083891997af8d25a2f99326
muthusubra <muthu.subramanian@digital.justice.gov.uk>
2023-01-04 16:41:33 +0000

    Resolved Merge Conflict

--- crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/prosecutionconcluded/listener/ProsecutionConcludedListener.java
+++ crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/prosecutionconcluded/listener/ProsecutionConcludedListener.java
@@ -2,6 +2,7 @@ package uk.gov.justice.laa.crime.crowncourt.prosecutionconcluded.listener;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @RequiredArgsConstructor
 @Component
+@ConditionalOnProperty(value = "feature.postMvp.enabled", havingValue = "true")
 public class ProsecutionConcludedListener {
     @JmsListener(destination = "${cloud-platform.aws.sqs.queue.prosecutionConcluded}", concurrency = "1")
     public void receive(@Payload final String message) {
