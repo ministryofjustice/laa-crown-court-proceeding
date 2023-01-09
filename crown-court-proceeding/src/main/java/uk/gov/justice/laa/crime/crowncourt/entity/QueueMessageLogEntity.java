e1a6b12b11233651e4a90cf288650ac1b12ed722
muthusubra <muthu.subramanian@digital.justice.gov.uk>
2023-01-09 09:45:38 +0000

    Updated Prosecution Concluded Listener Test

--- crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/entity/QueueMessageLogEntity.java
+++ crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/entity/QueueMessageLogEntity.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -15,18 +16,15 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "QUEUE_MESSAGE_LOG", schema = "crown_court_proceeding")
 public class QueueMessageLogEntity {
-
     @Id
     @Column(name = "TRANSACTION_UUID")
     private String transactionUUID;
     @Column(name = "LAA_TRANSACTION_ID")
     private String laaTransactionId;
-
     @Column(name = "MAAT_ID")
     private Integer maatId;
     @Column(name = "TYPE")
     private String type;
-    @Lob
     @Column(name = "MESSAGE")
     private byte[] message;
     @Column(name = "CREATED_TIME")
