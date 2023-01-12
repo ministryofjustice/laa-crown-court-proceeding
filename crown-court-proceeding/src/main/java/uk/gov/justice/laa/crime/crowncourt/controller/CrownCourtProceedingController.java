6e53f0f074fb4ab567dcdad7b4acb3aa15b8155b
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2023-01-12 14:31:21 +0000

    New unified DTO for all requests + renamed schemas & classes
    This simplifies request handling and reduces the number of builder classes required. The names of classes and schemas have been updated to better reflect their purpose.

--- crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/controller/CrownCourtProceedingController.java
+++ crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/controller/CrownCourtProceedingController.java
@@ -11,14 +11,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import uk.gov.justice.laa.crime.crowncourt.builder.ProcessCrownRepOrderRequestDTOBuilder;
-import uk.gov.justice.laa.crime.crowncourt.builder.CrownCourtApplicationRequestDTOBuilder;
-import uk.gov.justice.laa.crime.crowncourt.dto.ProcessCrownRepOrderRequestDTO;
+import uk.gov.justice.laa.crime.crowncourt.builder.CrownCourtDTOBuilder;
+import uk.gov.justice.laa.crime.crowncourt.dto.CrownCourtDTO;
 import uk.gov.justice.laa.crime.crowncourt.dto.ErrorDTO;
-import uk.gov.justice.laa.crime.crowncourt.model.ApiProcessCrownRepOrderRequest;
-import uk.gov.justice.laa.crime.crowncourt.model.ApiProcessCrownRepOrderResponse;
-import uk.gov.justice.laa.crime.crowncourt.model.ApiUpdateCrownCourtApplicationRequest;
-import uk.gov.justice.laa.crime.crowncourt.service.CrownCourtProceedingService;
+import uk.gov.justice.laa.crime.crowncourt.model.ApiProcessRepOrderRequest;
+import uk.gov.justice.laa.crime.crowncourt.model.ApiProcessRepOrderResponse;
+import uk.gov.justice.laa.crime.crowncourt.model.ApiUpdateApplicationRequest;
+import uk.gov.justice.laa.crime.crowncourt.service.ProceedingService;
 
 import javax.validation.Valid;
 import java.io.IOException;
@@ -31,17 +30,17 @@ import java.net.URISyntaxException;
 @Tag(name = "Crown Court Proceeding", description = "Rest API for Crown Court Proceeding.")
 public class CrownCourtProceedingController {
 
-    private final CrownCourtProceedingService crownCourtProceedingService;
+    private final ProceedingService proceedingService;
 
-    private ProcessCrownRepOrderRequestDTO preProcessRequest(ApiProcessCrownRepOrderRequest processCrownRepOrderRequest) {
-        return new ProcessCrownRepOrderRequestDTOBuilder().buildRequestDTO(processCrownRepOrderRequest);
+    private CrownCourtDTO preProcessRequest(ApiProcessRepOrderRequest request) {
+        return CrownCourtDTOBuilder.build(request);
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(description = "Process Crown Rep Order Data")
     @ApiResponse(responseCode = "200",
             content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = @Schema(implementation = ApiProcessCrownRepOrderRequest.class)
+                    schema = @Schema(implementation = ApiProcessRepOrderRequest.class)
             )
     )
     @ApiResponse(responseCode = "400",
@@ -56,16 +55,16 @@ public class CrownCourtProceedingController {
                     schema = @Schema(implementation = ErrorDTO.class)
             )
     )
-    public ResponseEntity<ApiProcessCrownRepOrderResponse> processCrownRepOrder(
+    public ResponseEntity<ApiProcessRepOrderResponse> processCrownRepOrder(
             @Parameter(description = "Process Crown Rep Order",
                     content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = ApiProcessCrownRepOrderRequest.class)
+                            schema = @Schema(implementation = ApiProcessRepOrderRequest.class)
                     )
-            ) @Valid @RequestBody ApiProcessCrownRepOrderRequest processCrownRepOrderRequest) {
+            ) @Valid @RequestBody ApiProcessRepOrderRequest request) {
 
-        ProcessCrownRepOrderRequestDTO requestDTO = preProcessRequest(processCrownRepOrderRequest);
+        CrownCourtDTO requestDTO = preProcessRequest(request);
         return ResponseEntity.ok(
-                crownCourtProceedingService.processCrownRepOrder(requestDTO)
+                proceedingService.processRepOrder(requestDTO)
         );
     }
 
@@ -84,9 +83,8 @@ public class CrownCourtProceedingController {
                     schema = @Schema(implementation = ErrorDTO.class)
             )
     )
-    public ResponseEntity<Object> updateCrownCourtActions(@Valid @RequestBody ApiUpdateCrownCourtApplicationRequest crownCourtApplicationRequest) {
-        crownCourtProceedingService.updateCrownCourtApplication(
-                new CrownCourtApplicationRequestDTOBuilder().buildRequestDTO(crownCourtApplicationRequest));
+    public ResponseEntity<Object> updateApplication(@Valid @RequestBody ApiUpdateApplicationRequest request) {
+        proceedingService.updateApplication(preProcessRequest(request));
         return ResponseEntity.ok().build();
     }
 
@@ -97,7 +95,7 @@ public class CrownCourtProceedingController {
     @ApiResponse(responseCode = "500", description = "Server Error.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class)))
     public ResponseEntity<Object> graphQLQuery() throws URISyntaxException, IOException {
         log.info("Make GraphQL Query Request");
-        return ResponseEntity.ok(crownCourtProceedingService.graphQLQuery());
+        return ResponseEntity.ok(proceedingService.graphQLQuery());
     }
 
 }
--- crown-court-proceeding/src/test/java/uk/gov/justice/laa/crime/crowncourt/service/CrownCourtProceedingServiceTest.java
+++ crown-court-proceeding/src/test/java/uk/gov/justice/laa/crime/crowncourt/service/CrownCourtProceedingServiceTest.java
@@ -9,14 +9,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.justice.laa.crime.crowncourt.data.builder.TestModelDataBuilder;
-import uk.gov.justice.laa.crime.crowncourt.dto.ProcessCrownRepOrderRequestDTO;
-import uk.gov.justice.laa.crime.crowncourt.dto.CrownCourtApplicationRequestDTO;
-import uk.gov.justice.laa.crime.crowncourt.model.ApiProcessCrownRepOrderResponse;
+import uk.gov.justice.laa.crime.crowncourt.dto.CrownCourtDTO;
+import uk.gov.justice.laa.crime.crowncourt.dto.maatcourtdata.UpdateRepOrderRequestDTO;
+import uk.gov.justice.laa.crime.crowncourt.model.ApiProcessRepOrderResponse;
 import uk.gov.justice.laa.crime.crowncourt.staticdata.enums.CaseType;
 import uk.gov.justice.laa.crime.crowncourt.staticdata.enums.MagCourtOutcome;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -28,26 +28,29 @@ class CrownCourtProceedingServiceTest {
     private SoftAssertions softly;
 
     @InjectMocks
-    private CrownCourtProceedingService crownCourtProceedingService;
+    private ProceedingService proceedingService;
 
     @Mock
     private RepOrderService repOrderService;
 
+    @Mock
+    private MaatCourtDataService maatCourtDataService;
+
     private void setupMockData() {
-        when(repOrderService.getRepDecision(any(ProcessCrownRepOrderRequestDTO.class)))
+        when(repOrderService.getRepDecision(any(CrownCourtDTO.class)))
                 .thenReturn(TestModelDataBuilder.getCrownCourtSummary());
-        when(repOrderService.determineCrownRepType(any(ProcessCrownRepOrderRequestDTO.class)))
+        when(repOrderService.determineCrownRepType(any(CrownCourtDTO.class)))
                 .thenReturn(TestModelDataBuilder.getCrownCourtSummary());
-        when(repOrderService.determineRepOrderDate(any(ProcessCrownRepOrderRequestDTO.class)))
+        when(repOrderService.determineRepOrderDate(any(CrownCourtDTO.class)))
                 .thenReturn(TestModelDataBuilder.getCrownCourtSummary());
     }
 
     @Test
-    void givenValidIndictableCase_whenProcessCrownRepOrderIsInvoked_validResponseIsReturned() {
-        ProcessCrownRepOrderRequestDTO requestDTO = TestModelDataBuilder.getProcessCrownRepOrderRequestDTO();
+    void givenValidIndictableCase_whenProcessRepOrderIsInvoked_validResponseIsReturned() {
+        CrownCourtDTO requestDTO = TestModelDataBuilder.getCrownCourtDTO();
         requestDTO.setCaseType(CaseType.INDICTABLE);
         setupMockData();
-        ApiProcessCrownRepOrderResponse response = crownCourtProceedingService.processCrownRepOrder(requestDTO);
+        ApiProcessRepOrderResponse response = proceedingService.processRepOrder(requestDTO);
 
         softly.assertThat(response.getRepOrderDecision())
                 .isEqualTo(TestModelDataBuilder.MOCK_DECISION);
@@ -57,11 +60,11 @@ class CrownCourtProceedingServiceTest {
     }
 
     @Test
-    void givenValidCCAlreadyCase_whenProcessCrownRepOrderIsInvoked_validResponseIsReturned() {
-        ProcessCrownRepOrderRequestDTO requestDTO = TestModelDataBuilder.getProcessCrownRepOrderRequestDTO();
+    void givenValidCCAlreadyCase_whenProcessRepOrderIsInvoked_validResponseIsReturned() {
+        CrownCourtDTO requestDTO = TestModelDataBuilder.getCrownCourtDTO();
         requestDTO.setCaseType(CaseType.CC_ALREADY);
         setupMockData();
-        ApiProcessCrownRepOrderResponse response = crownCourtProceedingService.processCrownRepOrder(requestDTO);
+        ApiProcessRepOrderResponse response = proceedingService.processRepOrder(requestDTO);
 
         softly.assertThat(response.getRepOrderDecision())
                 .isEqualTo(TestModelDataBuilder.MOCK_DECISION);
@@ -71,11 +74,11 @@ class CrownCourtProceedingServiceTest {
     }
 
     @Test
-    void givenValidAppealCCCase_whenProcessCrownRepOrderIsInvoked_validResponseIsReturned() {
-        ProcessCrownRepOrderRequestDTO requestDTO = TestModelDataBuilder.getProcessCrownRepOrderRequestDTO();
+    void givenValidAppealCCCase_whenProcessRepOrderIsInvoked_validResponseIsReturned() {
+        CrownCourtDTO requestDTO = TestModelDataBuilder.getCrownCourtDTO();
         requestDTO.setCaseType(CaseType.APPEAL_CC);
         setupMockData();
-        ApiProcessCrownRepOrderResponse response = crownCourtProceedingService.processCrownRepOrder(requestDTO);
+        ApiProcessRepOrderResponse response = proceedingService.processRepOrder(requestDTO);
 
         softly.assertThat(response.getRepOrderDecision())
                 .isEqualTo(TestModelDataBuilder.MOCK_DECISION);
@@ -85,11 +88,11 @@ class CrownCourtProceedingServiceTest {
     }
 
     @Test
-    void givenValidCommittalCase_whenProcessCrownRepOrderIsInvoked_validResponseIsReturned() {
-        ProcessCrownRepOrderRequestDTO requestDTO = TestModelDataBuilder.getProcessCrownRepOrderRequestDTO();
+    void givenValidCommittalCase_whenProcessRepOrderIsInvoked_validResponseIsReturned() {
+        CrownCourtDTO requestDTO = TestModelDataBuilder.getCrownCourtDTO();
         requestDTO.setCaseType(CaseType.COMMITAL);
         setupMockData();
-        ApiProcessCrownRepOrderResponse response = crownCourtProceedingService.processCrownRepOrder(requestDTO);
+        ApiProcessRepOrderResponse response = proceedingService.processRepOrder(requestDTO);
 
         softly.assertThat(response.getRepOrderDecision())
                 .isEqualTo(TestModelDataBuilder.MOCK_DECISION);
@@ -99,12 +102,12 @@ class CrownCourtProceedingServiceTest {
     }
 
     @Test
-    void givenValidEitherWayCaseWithCommittedMagOutcome_whenProcessCrownRepOrderIsInvoked_validResponseIsReturned() {
-        ProcessCrownRepOrderRequestDTO requestDTO = TestModelDataBuilder.getProcessCrownRepOrderRequestDTO();
+    void givenValidEitherWayCaseWithCommittedMagOutcome_whenProcessRepOrderIsInvoked_validResponseIsReturned() {
+        CrownCourtDTO requestDTO = TestModelDataBuilder.getCrownCourtDTO();
         requestDTO.setCaseType(CaseType.EITHER_WAY);
         requestDTO.setMagCourtOutcome(MagCourtOutcome.COMMITTED);
         setupMockData();
-        ApiProcessCrownRepOrderResponse response = crownCourtProceedingService.processCrownRepOrder(requestDTO);
+        ApiProcessRepOrderResponse response = proceedingService.processRepOrder(requestDTO);
 
         softly.assertThat(response.getRepOrderDecision())
                 .isEqualTo(TestModelDataBuilder.MOCK_DECISION);
@@ -114,12 +117,12 @@ class CrownCourtProceedingServiceTest {
     }
 
     @Test
-    void givenValidEitherWayCaseWithCommittedForTrailMagOutcome_whenProcessCrownRepOrderIsInvoked_validResponseIsReturned() {
-        ProcessCrownRepOrderRequestDTO requestDTO = TestModelDataBuilder.getProcessCrownRepOrderRequestDTO();
+    void givenValidEitherWayCaseWithCommittedForTrailMagOutcome_whenProcessRepOrderIsInvoked_validResponseIsReturned() {
+        CrownCourtDTO requestDTO = TestModelDataBuilder.getCrownCourtDTO();
         requestDTO.setCaseType(CaseType.EITHER_WAY);
         requestDTO.setMagCourtOutcome(MagCourtOutcome.COMMITTED_FOR_TRIAL);
         setupMockData();
-        ApiProcessCrownRepOrderResponse response = crownCourtProceedingService.processCrownRepOrder(requestDTO);
+        ApiProcessRepOrderResponse response = proceedingService.processRepOrder(requestDTO);
 
         softly.assertThat(response.getRepOrderDecision())
                 .isEqualTo(TestModelDataBuilder.MOCK_DECISION);
@@ -129,12 +132,12 @@ class CrownCourtProceedingServiceTest {
     }
 
     @Test
-    void givenValidEitherWayCaseWithSentForTrailMagOutcome_whenProcessCrownRepOrderIsInvoked_validResponseIsReturned() {
-        ProcessCrownRepOrderRequestDTO requestDTO = TestModelDataBuilder.getProcessCrownRepOrderRequestDTO();
+    void givenValidEitherWayCaseWithSentForTrailMagOutcome_whenProcessRepOrderIsInvoked_validResponseIsReturned() {
+        CrownCourtDTO requestDTO = TestModelDataBuilder.getCrownCourtDTO();
         requestDTO.setCaseType(CaseType.EITHER_WAY);
         requestDTO.setMagCourtOutcome(MagCourtOutcome.SENT_FOR_TRIAL);
         setupMockData();
-        ApiProcessCrownRepOrderResponse response = crownCourtProceedingService.processCrownRepOrder(requestDTO);
+        ApiProcessRepOrderResponse response = proceedingService.processRepOrder(requestDTO);
 
         softly.assertThat(response.getRepOrderDecision())
                 .isEqualTo(TestModelDataBuilder.MOCK_DECISION);
@@ -144,12 +147,12 @@ class CrownCourtProceedingServiceTest {
     }
 
     @Test
-    void givenValidEitherWayCaseWithAppealToCCMagOutcome_whenCheckCrownCourtActionsIsInvoked_validResponseIsReturned() {
-        ProcessCrownRepOrderRequestDTO requestDTO = TestModelDataBuilder.getProcessCrownRepOrderRequestDTO();
+    void givenValidEitherWayCaseWithAppealToCCMagOutcome_whenProcessRepOrderIsInvoked_validResponseIsReturned() {
+        CrownCourtDTO requestDTO = TestModelDataBuilder.getCrownCourtDTO();
         requestDTO.setCaseType(CaseType.EITHER_WAY);
         requestDTO.setMagCourtOutcome(MagCourtOutcome.APPEAL_TO_CC);
         setupMockData();
-        ApiProcessCrownRepOrderResponse response = crownCourtProceedingService.processCrownRepOrder(requestDTO);
+        ApiProcessRepOrderResponse response = proceedingService.processRepOrder(requestDTO);
 
         softly.assertThat(response.getRepOrderDecision())
                 .isEqualTo(TestModelDataBuilder.MOCK_DECISION);
@@ -159,26 +162,27 @@ class CrownCourtProceedingServiceTest {
     }
 
     @Test
-    void givenSummaryOnlyCase_whenProcessCrownRepOrderIsInvoked_emptyResponseIsReturned() {
-        ProcessCrownRepOrderRequestDTO requestDTO = TestModelDataBuilder.getProcessCrownRepOrderRequestDTO();
-        assertThat(crownCourtProceedingService.processCrownRepOrder(requestDTO))
-                .isEqualTo(new ApiProcessCrownRepOrderResponse());
+    void givenSummaryOnlyCase_whenProcessRepOrderIsInvoked_emptyResponseIsReturned() {
+        CrownCourtDTO requestDTO = TestModelDataBuilder.getCrownCourtDTO();
+        assertThat(proceedingService.processRepOrder(requestDTO))
+                .isEqualTo(new ApiProcessRepOrderResponse());
     }
 
     @Test
-    void givenEitherWayCaseWithResolvedMagOutcome_whenProcessCrownRepOrderIsInvoked_emptyResponseIsReturned() {
-        ProcessCrownRepOrderRequestDTO requestDTO = TestModelDataBuilder.getProcessCrownRepOrderRequestDTO();
+    void givenEitherWayCaseWithResolvedMagOutcome_whenProcessRepOrderIsInvoked_emptyResponseIsReturned() {
+        CrownCourtDTO requestDTO = TestModelDataBuilder.getCrownCourtDTO();
         requestDTO.setCaseType(CaseType.EITHER_WAY);
         requestDTO.setMagCourtOutcome(MagCourtOutcome.RESOLVED_IN_MAGS);
-        assertThat(crownCourtProceedingService.processCrownRepOrder(requestDTO))
-                .isEqualTo(new ApiProcessCrownRepOrderResponse());
+        assertThat(proceedingService.processRepOrder(requestDTO))
+                .isEqualTo(new ApiProcessRepOrderResponse());
     }
 
     @Test
-    void givenCCApplication_whenUpdateCrownCourtApplicationIsInvoked_thenSentenceOrderDateIsPersisted() {
-        CrownCourtApplicationRequestDTO requestDTO = TestModelDataBuilder.getCrownCourtApplicationRequestDTO();
-        crownCourtProceedingService.updateCrownCourtApplication(requestDTO);
-        verify(repOrderService).updateCCSentenceOrderDate(any(CrownCourtApplicationRequestDTO.class));
+    void givenCCApplication_whenUpdateApplicationIsInvoked_thenSentenceOrderDateIsPersisted() {
+        CrownCourtDTO requestDTO = TestModelDataBuilder.getCrownCourtDTO();
+        proceedingService.updateApplication(requestDTO);
+        verify(repOrderService).updateCCSentenceOrderDate(any(CrownCourtDTO.class));
+        verify(maatCourtDataService).updateRepOrder(any(UpdateRepOrderRequestDTO.class), anyString());
     }
 
 
