247900e0e6a2ed990defc5df002e51ca54902c13
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2023-01-12 14:35:51 +0000

    Migrated business logic from the update_cc_application stored procedure
    This involves calling the Court Data API to update the Rep Order information stored in MAAT DB. Therefore, the request DTO was updated to match recent changes to the API and a new builder class was added.

---
+++ crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/builder/UpdateRepOrderDTOBuilder.java
@@ -0,0 +1,41 @@
+package uk.gov.justice.laa.crime.crowncourt.builder;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+import uk.gov.justice.laa.crime.crowncourt.dto.CrownCourtDTO;
+import uk.gov.justice.laa.crime.crowncourt.dto.maatcourtdata.UpdateRepOrderRequestDTO;
+import uk.gov.justice.laa.crime.crowncourt.model.ApiCrownCourtSummary;
+import uk.gov.justice.laa.crime.crowncourt.model.ApiPaymentDetails;
+
+import java.time.LocalDateTime;
+
+import static java.util.Optional.ofNullable;
+
+@Component
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UpdateRepOrderDTOBuilder {
+
+    public static UpdateRepOrderRequestDTO build(CrownCourtDTO crownCourtDTO) {
+        ApiPaymentDetails paymentDetails = crownCourtDTO.getPaymentDetails();
+        ApiCrownCourtSummary crownCourtSummary = crownCourtDTO.getCrownCourtSummary();
+        return UpdateRepOrderRequestDTO.builder()
+                .repId(crownCourtDTO.getRepId())
+                .crownRepOrderDecision(crownCourtSummary.getRepOrderDecision())
+                .crownRepOrderType(crownCourtSummary.getRepType())
+                .crownWithdrawalDate(
+                        ofNullable(crownCourtSummary.getWithdrawalDate())
+                                .map(LocalDateTime::toLocalDate)
+                                .orElse(null)
+                )
+                .evidenceFeeLevel(crownCourtSummary.getEvidenceFeeLevel())
+                .isImprisoned(crownCourtDTO.getIsImprisoned())
+                .bankAccountName(paymentDetails.getBankAccountName())
+                .bankAccountNo(paymentDetails.getBankAccountNo())
+                .paymentMethod(paymentDetails.getPaymentMethod())
+                .preferredPaymentDay(paymentDetails.getPreferredPaymentDay())
+                .sortCode(paymentDetails.getSortCode())
+                .userModified(crownCourtDTO.getUserSession().getUserName())
+                .build();
+    }
+}
