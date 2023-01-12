8c976a831d73ba0f883f455fa135835fde681075
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2023-01-12 14:41:44 +0000

    Refactored ProceedingService to reduce complexity

--- crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/service/ProceedingService.java
+++ crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/service/ProceedingService.java
@@ -13,7 +13,7 @@ import uk.gov.justice.laa.crime.crowncourt.staticdata.enums.MagCourtOutcome;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @Service
@@ -23,23 +23,15 @@ public class ProceedingService {
     private final RepOrderService repOrderService;
     private final MaatCourtDataService maatCourtDataService;
 
-    private final List<CaseType> caseTypes = List.of(
-            CaseType.INDICTABLE,
-            CaseType.CC_ALREADY,
-            CaseType.APPEAL_CC,
-            CaseType.COMMITAL
-    );
-    private final List<MagCourtOutcome> magCourtOutcomes = List.of(
-            MagCourtOutcome.COMMITTED_FOR_TRIAL,
-            MagCourtOutcome.SENT_FOR_TRIAL,
-            MagCourtOutcome.COMMITTED,
-            MagCourtOutcome.APPEAL_TO_CC
+    private final Set<CaseType> caseTypes = Set.of(
+            CaseType.INDICTABLE, CaseType.CC_ALREADY, CaseType.APPEAL_CC, CaseType.COMMITAL
     );
 
     public ApiProcessRepOrderResponse processRepOrder(CrownCourtDTO dto) {
         ApiProcessRepOrderResponse apiProcessCrownRepOrderResponse = new ApiProcessRepOrderResponse();
         if (caseTypes.contains(dto.getCaseType()) ||
-                (dto.getCaseType() == CaseType.EITHER_WAY && magCourtOutcomes.contains(dto.getMagCourtOutcome()))) {
+                (dto.getCaseType() == CaseType.EITHER_WAY &&
+                        !dto.getMagCourtOutcome().equals(MagCourtOutcome.RESOLVED_IN_MAGS))) {
             repOrderService.getRepDecision(dto);
             repOrderService.determineCrownRepType(dto);
             ApiCrownCourtSummary apiCrownCourtSummary = repOrderService.determineRepOrderDate(dto);
@@ -60,7 +52,7 @@ public class ProceedingService {
     public Object graphQLQuery() throws URISyntaxException, IOException {
         log.info("Start");
         Object obj = maatCourtDataService.getRepOrderByFilter("5639461", "false");
-        log.info("Response :" + obj.toString() );
+        log.info("Response :" + obj.toString());
         return obj;
     }
 
