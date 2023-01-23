54211cada59c2c38d27df9422fca9c6074d20dc0
loganathanMOJ <110454701+loganathanMOJ@users.noreply.github.com>
2023-01-23 14:01:05 +0000

    LCAM-258-client class added

---
+++ crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/model/laastatus/Address.java
@@ -0,0 +1,36 @@
+package uk.gov.justice.laa.crime.crowncourt.model.laastatus;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    @SerializedName("address1")
+    @Expose
+    private String address1;
+    @SerializedName("address2")
+    private String address2;
+    @SerializedName("address3")
+    @Expose
+    private String address3;
+    @SerializedName("address4")
+    @Expose
+    private String address4;
+    @SerializedName("address5")
+    @Expose
+    private String address5;
+    @SerializedName("postcode")
+    @Expose
+    private String postcode;
+
+}
