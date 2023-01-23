190456a2372ea6b6e5f9a6a32e600d0e93880bff
loganathanMOJ <110454701+loganathanMOJ@users.noreply.github.com>
2023-01-23 14:01:05 +0000

    LCAM-258-client class added

---
+++ crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/client/CourtDataAdapterClient.java
@@ -0,0 +1,73 @@
+package uk.gov.justice.laa.crime.crowncourt.client;
+
+import com.google.gson.GsonBuilder;
+import io.sentry.Sentry;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import uk.gov.justice.laa.crime.crowncourt.config.CourtDataAdapterClientConfig;
+import uk.gov.justice.laa.crime.crowncourt.enums.MessageType;
+import uk.gov.justice.laa.crime.crowncourt.exception.CCPDataException;
+import uk.gov.justice.laa.crime.crowncourt.model.laastatus.LaaStatusUpdate;
+import uk.gov.justice.laa.crime.crowncourt.service.QueueMessageLogService;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CourtDataAdapterClient {
+
+    @Autowired
+    @Qualifier("cdaOAuth2WebClient")
+    private final WebClient webClient;
+
+    private final GsonBuilder gsonBuilder;
+
+    private final QueueMessageLogService queueMessageLogService;
+
+    private final CourtDataAdapterClientConfig courtDataAdapterClientConfig;
+
+    /**
+     * @param laaStatusUpdate laa status value
+     */
+    public void postLaaStatus(LaaStatusUpdate laaStatusUpdate, Map<String,String> headers) {
+
+
+        final String laaStatusUpdateJson = gsonBuilder.create().toJson(laaStatusUpdate);
+        queueMessageLogService.createLog(MessageType.LAA_STATUS_UPDATE,laaStatusUpdateJson);
+
+        log.info("Post Laa status to CDA.");
+        WebClient.ResponseSpec clientResponse =
+                webClient
+                        .post()
+                        .uri(uriBuilder -> uriBuilder.path(courtDataAdapterClientConfig.getLaaStatusUrl()).build())
+                        .headers(httpHeaders -> httpHeaders.setAll(headers))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromValue(laaStatusUpdateJson))
+                        .retrieve();
+
+        log.info("LAA status update posted {}", Optional.of( clientResponse.toBodilessEntity().block().getStatusCode() ));
+    }
+
+    public void triggerHearingProcessing(UUID hearingId, String laaTransactionId) {
+        log.info("Triggering processing for hearing '{}' via court data adapter.", hearingId);
+        webClient
+                .get()
+                .uri(uriBuilder ->
+                        uriBuilder.path(courtDataAdapterClientConfig.getHearingUrl()).queryParam("publish_to_queue", true).build(hearingId))
+                .headers(httpHeaders -> httpHeaders.setAll(Map.of("X-Request-ID", laaTransactionId)))
+                .retrieve().toBodilessEntity()
+                .doOnError(Sentry::captureException)
+                .onErrorMap(error -> new CCPDataException(String.format("Error triggering CDA processing for hearing '%s'.%s", hearingId, error.getMessage())))
+                .doOnSuccess(response -> log.info("Processing trigger successfully for hearing '{}'", hearingId))
+                .block();
+    }
+}
