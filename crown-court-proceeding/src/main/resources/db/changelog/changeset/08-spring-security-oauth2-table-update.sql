a45784019591ce69cef7dd50b2bc19dbd8c4290d
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2023-05-11 16:35:33 +0100

    Fix SQL issue preventing the application starting

--- crown-court-proceeding/src/main/resources/db/changelog/changeset/08-spring-security-oauth2-table-update.sql
+++ crown-court-proceeding/src/main/resources/db/changelog/changeset/08-spring-security-oauth2-table-update.sql
@@ -2,5 +2,5 @@
 --changeset matthewh:04-spring-security-oauth2-table-update
 
 ALTER TABLE oauth2_authorization
-    ADD authorized_scopes varchar(1000) DEFAULT NULL AFTER authorization_grant_type;
+    ADD COLUMN authorized_scopes varchar(1000) DEFAULT NULL;
 
