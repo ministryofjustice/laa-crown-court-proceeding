1ac4e142ef9d4d422a5ea62c1653245c22e0041a
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2024-03-06 17:12:05 +0000

    Fix misleading log message and remove unused imports

--- crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/service/CrimeEvidenceDataService.java
+++ crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/service/CrimeEvidenceDataService.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import uk.gov.justice.laa.crime.commons.client.RestAPIClient;
-import uk.gov.justice.laa.crime.crowncourt.common.Constants;
 import uk.gov.justice.laa.crime.crowncourt.config.ServicesConfiguration;
-import uk.gov.justice.laa.crime.crowncourt.dto.maatcourtdata.RepOrderCCOutcomeDTO;
 import uk.gov.justice.laa.crime.crowncourt.model.ApiCalculateEvidenceFeeRequest;
 import uk.gov.justice.laa.crime.crowncourt.model.ApiCalculateEvidenceFeeResponse;
 
@@ -22,7 +20,7 @@ public class CrimeEvidenceDataService {
     @Qualifier("evidenceApiClient")
     private final RestAPIClient evidenceAPIClient;
     private final ServicesConfiguration configuration;
-    private static final String RESPONSE_STRING = "Response from Court Data API: {}";
+    private static final String RESPONSE_STRING = "Response from the Evidence service: {}";
 
     public ApiCalculateEvidenceFeeResponse getCalEvidenceFee(ApiCalculateEvidenceFeeRequest evidenceFeeRequest) {
         ApiCalculateEvidenceFeeResponse response = evidenceAPIClient.post(
