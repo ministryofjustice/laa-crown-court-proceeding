70c5b81daa2123b6bc3f4cf82907d1629e2205bb
Ross-Nation <139126932+Ross-Nation@users.noreply.github.com>
2025-08-14 15:01:22 +0100

    Remove "public" modifier in appeal outcome helper tests

--- crown-court-proceeding/src/test/java/uk/gov/justice/laa/crime/crowncourt/prosecution_concluded/helper/CalculateAppealOutcomeHelperTest.java
+++ crown-court-proceeding/src/test/java/uk/gov/justice/laa/crime/crowncourt/prosecution_concluded/helper/CalculateAppealOutcomeHelperTest.java
@@ -3,23 +3,23 @@ package uk.gov.justice.laa.crime.crowncourt.prosecution_concluded.helper;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-public class CalculateAppealOutcomeHelperTest {
+class CalculateAppealOutcomeHelperTest {
   private CalculateAppealOutcomeHelper calculateAppealOutcomeHelper = new CalculateAppealOutcomeHelper();
 
   @Test
-  public void testPartSuccess() {
+  void testPartSuccess() {
     assertEquals("PART SUCCESS", calculateAppealOutcomeHelper.calculate("AACD AASA"));
     assertEquals("PART SUCCESS", calculateAppealOutcomeHelper.calculate("AASA AACD"));
   }
 
   @Test
-  public void testSuccessful() {
+  void testSuccessful() {
     assertEquals("SUCCESSFUL", calculateAppealOutcomeHelper.calculate("AACA"));
     assertEquals("SUCCESSFUL", calculateAppealOutcomeHelper.calculate("AASA"));
   }
 
   @Test
-  public void testUnsuccessful() {
+  void testUnsuccessful() {
     assertEquals("UNSUCCESSFUL", calculateAppealOutcomeHelper.calculate("APA"));
     assertEquals("UNSUCCESSFUL", calculateAppealOutcomeHelper.calculate("AW"));
     assertEquals("UNSUCCESSFUL", calculateAppealOutcomeHelper.calculate("AACD"));
