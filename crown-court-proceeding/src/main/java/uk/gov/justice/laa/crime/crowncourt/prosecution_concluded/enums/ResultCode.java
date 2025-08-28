628d4235bdfdcf45e8749ab79e9012cc652ee2bb
loganathanMOJ <110454701+loganathanMOJ@users.noreply.github.com>
2025-08-28 16:44:07 +0100

    added enum

---
+++ crown-court-proceeding/src/main/java/uk/gov/justice/laa/crime/crowncourt/prosecution_concluded/enums/ResultCode.java
@@ -0,0 +1,16 @@
+package uk.gov.justice.laa.crime.crowncourt.prosecution_concluded.enums;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public enum ResultCode {
+    APA,
+    AW,
+    AACD,
+    ASV,
+    AACA,
+    AASD,
+    AASA,
+    ACSD
+
+}
